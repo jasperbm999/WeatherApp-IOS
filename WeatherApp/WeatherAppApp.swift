7cd705273743f84c963d8fc800cdfd72c48d708b
jasperbm999 <97184854+jasperbm999@users.noreply.github.com>
2023-04-17 08:01:33 +0200

    Initial commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Jasper Broekmans on 04/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
